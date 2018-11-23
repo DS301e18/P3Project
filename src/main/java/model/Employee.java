3111c1dbc41d4e730bdfbc040294ef2e82e594d3
mhoang17 <mhoang17@github.com>
2018-11-23 12:27:26 +0100

    New project again

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,83 @@
+package model;
+
+public class Employee {
+
+    private int id;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private int role;
+    // private boolean state;
+
+
+    public Employee(int id, String username, String password, String firstname, String lastname, int role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.role = role;
+    }
+
+    public Employee(String firstname, String lastname, int role) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.role = role;
+    }
+
+    public Employee() {
+    }
+
+    public void withUsername(String username){
+        setUsername(username);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+}
