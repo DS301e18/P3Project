b3a04c404949a007dde5edf1071c8927b77f672e
Benis <tander16@student.aau.dk>
2018-11-28 12:36:09 +0100

    BatchTest created

---
+++ src/main/test/model/BatchTest.java
@@ -0,0 +1,15 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BatchTest {
+
+    @Test
+    void takeFromBatch() {
+
+        
+
+    }
+}
