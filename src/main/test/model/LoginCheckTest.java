a3dd0ee1f5d077a2050075b38757e0c0cf425212
mhoang17 <mhoang17@github.com>
2018-11-28 12:09:41 +0100

    SessionFactoryCfg added again Test for LoginCheck Small errors fixed

---
+++ src/main/test/model/LoginCheckTest.java
@@ -0,0 +1,39 @@
+package model;
+
+import org.hibernate.HibernateException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginCheckTest {
+
+    //Tester om funktionen kan finde en bruger i databasen
+    @Test
+    void check() {
+        try{
+            LoginCheck test1 = new LoginCheck();
+            boolean result1 = test1.check("admin", "admin123");
+            assertEquals(true, result1);
+        }
+        catch (HibernateException e){
+            System.out.println("Something went wrong");
+            e.printStackTrace();
+        }
+
+    }
+
+    //Tester om initializere objekter er indentisk med databaser.
+    @Test
+    void getEmployee() {
+        LoginCheck test2 = new LoginCheck();
+        test2.check("admin", "admin123");
+        Employee emp1 = new Employee();
+        emp1.setFirstname("Halleluja");
+        emp1.setLastname("My god");
+        emp1.setPassword("admin123");
+        emp1.setRole("Manager");
+        emp1.setUsername("admin");
+        emp1.setId(9);
+        assertEquals(emp1, test2.getEmployee());
+    }
+}
