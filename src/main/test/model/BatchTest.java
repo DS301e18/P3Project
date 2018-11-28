529b3260d93ac959ad117b882d9f87008b7f31c9
Benis <tander16@student.aau.dk>
2018-11-28 14:34:44 +0100

    BatchTest first test created, but not working due to error in SFC

--- src/main/test/model/BatchTest.java
+++ src/main/test/model/BatchTest.java
@@ -1,6 +1,12 @@
 package model;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.junit.jupiter.api.Test;
+
+import javax.persistence.criteria.From;
+import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,8 +14,25 @@ class BatchTest {
 
     @Test
     void takeFromBatch() {
+        Session session = new SessionFactoryCfg().createSessionFactory().openSession();
 
-        
+
+        Batch batchTest = new Batch();
+        batchTest.setBatchNumber("qw12");
+
+        try {
+
+            List<Batch> batchList = session.createQuery("FROM Batch ").list();
+
+            for(Batch batch : batchList){
+                if (batch.getBatchNumber().equals("qw12")){ assert true;}
+            }
+
+        } catch (HibernateException e){
+            System.out.println("PIS");
+        }finally {
+            session.close();
+        }
 
     }
 }
