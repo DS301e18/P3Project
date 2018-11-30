b996b52fc2ce61870a1852475205f743affb70e9
mismusen <32872317+mismusen@users.noreply.github.com>
2018-11-30 12:08:02 +0100

    Getters og setters er tilføjet Tomme metoder til funktionaliteter

---
+++ src/main/java/controller/SystemAdministratorController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import model.Manager;
+import model.Restaurant;
+
+public class SystemAdministratorController {
+    private Restaurant restaurant;
+    private Manager manager;
+    private String restaurantName;
+
+
+
+    public void addManager(Restaurant restaurant, Manager manager){
+
+    }
+
+    public void removeManager(Restaurant restaurant, Manager manager){
+
+    }
+
+    public void addRestaurant(){
+
+    }
+
+    public void removeRestaurant(Restaurant restaurant){
+
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+}
