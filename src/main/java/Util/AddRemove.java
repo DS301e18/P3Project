51840e5c5f65319ca0ffaada4cba2e68e9ef70cc
Benis <tander16@student.aau.dk>
2018-12-04 11:54:26 +0100

    Lavede første udkast af removeObject

--- src/main/java/Util/AddRemove.java
+++ src/main/java/Util/AddRemove.java
@@ -8,7 +8,7 @@ import org.hibernate.Transaction;
 
 public class AddRemove {
 
-    public <T> void addObject(T object){
+    protected <T> void addObject(T object) {
 
         SessionFactory sessionFactory = new SessionFactoryCfg().getSessionFactory();
         Session session = sessionFactory.openSession();
@@ -22,13 +22,37 @@ public class AddRemove {
 
             transaction.commit();
 
-        } catch (HibernateException e){
+        } catch (HibernateException e) {
             System.out.println("Could not save the object");
             e.printStackTrace();
 
-        }finally {
+        } finally {
             session.close();
 
         }
+    }
+
+    private <T> void removeObject(T object) {
+
+        SessionFactory sessionFactory = new SessionFactoryCfg().getSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction;
+
+        try {
+            transaction = session.beginTransaction();
+
+            session.delete(object);
+
+            transaction.commit();
+        } catch (HibernateException e) {
+            System.out.println("Could not save the object");
+            e.printStackTrace();
+
+        } finally {
+            session.close();
+
+        }
+
     }
 }
