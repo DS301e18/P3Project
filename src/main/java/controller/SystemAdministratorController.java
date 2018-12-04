3d4ac72d05dd8a7a006da89afc1df029373a30d1
Trja17 <45165051+Trja17@users.noreply.github.com>
2018-12-04 12:39:52 +0100

    En masse add metoder som skal ændres..

--- src/main/java/controller/SystemAdministratorController.java
+++ src/main/java/controller/SystemAdministratorController.java
@@ -24,7 +24,7 @@ public class SystemAdministratorController {
 
 
     /**Methods**/
-    public void addManager(Manager manager){
+    /*public void addManager(Manager manager){
         SessionFactory sessionFactory = new SessionFactoryCfg().createSessionFactory();
         Session session = sessionFactory.openSession();
 
@@ -42,7 +42,7 @@ public class SystemAdministratorController {
         } finally {
             session.close();
         }
-    }
+    }*/
 
     public void removeManager(Manager manager){
 
@@ -72,7 +72,8 @@ public class SystemAdministratorController {
 
     }
 
-    public void addRestaurant(String restaurantName){
+
+    /*public void addRestaurant(String restaurantName){
 
         restaurant = new Restaurant();
         restaurant.setName(restaurantName);
@@ -94,7 +95,7 @@ public class SystemAdministratorController {
             session.close();
         }
 
-    }
+    }*/
 
     public void removeRestaurant(String restaurantName){
 
