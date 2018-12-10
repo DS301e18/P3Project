2af8addf48728a5c6dcf60a63213f9c93da382a3
mhoang17 <mhoang17@github.com>
2018-12-10 16:32:11 +0100

    Remove manager works

---
+++ src/main/java/controller/SuperuserController/EditManagerController.java
@@ -0,0 +1,37 @@
+package controller.SuperuserController;
+
+import model.Employee;
+import model.Manager;
+import model.SystemAdministrator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/EditManager")
+public class EditManagerController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int employeeID = Integer.parseInt(req.getParameter("managerID"));
+        String firstName = req.getParameter("fName");
+        String lastName = req.getParameter("lName");
+        String username = req.getParameter("uName");
+        String password = req.getParameter("pWord");
+
+        List<Manager> managerList = SystemAdministrator.collectManagers();
+
+        Employee manager = managerList.get(employeeID);
+
+        manager.setFirstName(firstName);
+        manager.setLastName(lastName);
+        manager.setUsername(username);
+        manager.setPassword(password);
+
+        resp.sendRedirect("superuserWebpanel.jsp");
+    }
+}
--- src/main/java/controller/SuperuserController/ManagerController.java
+++ src/main/java/controller/SuperuserController/ManagerController.java
@@ -1,7 +1,9 @@
 package controller.SuperuserController;
 
 
+import model.Employee;
 import model.Manager;
+import model.SystemAdministrator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/Manager")
 public class ManagerController extends HttpServlet {
@@ -23,6 +26,20 @@ public class ManagerController extends HttpServlet {
         String password = req.getParameter("password");
 
         new Manager(username, password, firstName, lastName);
+
+        resp.sendRedirect("superuserWebpanel.jsp");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        int managerID = Integer.parseInt(req.getParameter("manIDDelete"));
+
+        List<Manager> managerList = SystemAdministrator.collectManagers();
+
+        Manager manager = managerList.get(managerID);
+
+        manager.removeManager();
 
         resp.sendRedirect("superuserWebpanel.jsp");
     }
