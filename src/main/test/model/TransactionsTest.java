856c8f67f7696bd0773f6bf2b7ba56917a2bbf95
Trja17 <45165051+Trja17@users.noreply.github.com>
2018-12-11 00:07:25 +0100

    Fixed broken test - Still misses to get its transaction removed again.

--- src/main/test/model/TransactionsTest.java
+++ src/main/test/model/TransactionsTest.java
@@ -1,9 +1,13 @@
 package model;
 
 import org.hibernate.*;
+import org.hibernate.query.Query;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import relationClasses.ProductBatch;
+import relationClasses.RestaurantEmployee;
 import relationClasses.RestaurantStorage;
+import relationClasses.StorageProduct;
 import util.SessionFactoryCfg;
 
 import java.math.BigDecimal;
@@ -11,89 +15,37 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionsTest {
-
-    Employee e;
-    Batch b;
-
-    @BeforeEach
-    void before(){
-        //Instantiates the objects that are input-parameters to the method
-        b = new Batch();
-        Product p = new Product();
-
-        //Setting random values for the objects
-        p.setName("Pepsi Max");
-        p.setPrice(BigDecimal.valueOf(23));
-        p.setBatchSize(3);
-
-        b.setBatchNumber("213123");
-        b.setProduct(p);
-        b.setRemainingInBox(p.getBatchSize());
-
-    }
+    Session session = new SessionFactoryCfg().createSessionFactory().openSession();
 
     @Test
     void registerTransactionTest() {
-
-        new SessionFactoryCfg().createSessionFactory();
+        session.getSessionFactory();
 
         Transactions transactions = new Transactions();
-        Transaction transaction;
 
-        //e = new Employee("Kaj", "Kajesen", "KajKajKaj", "1234kaj");
+        Restaurant restaurant = new Restaurant("Test Restaurant");
+        Employee employee = new Employee("Test12", "Test12", "Test Person", "Test Name", "Medarbejder");
+        Storage storage = new Storage("Test Lager");
+        Product product = new Product("Product Test", 10, BigDecimal.valueOf(10));
+        Batch batch = new Batch(product, "batchNumberTest", 10);
 
-        Restaurant restaurant = new Restaurant("ålleren");
+        RestaurantStorage restaurantStorage = new RestaurantStorage(restaurant.getId(), storage.getId());
+        RestaurantEmployee restaurantEmployee = new RestaurantEmployee(restaurant.getId(), employee.getId());
+        StorageProduct storageProduct = new StorageProduct(storage.getId(), product.getId());
+        ProductBatch productBatch = new ProductBatch(product.getId(), batch.getId());
 
-        Storage s = new Storage("Mas");
+        transactions.registerTransaction(storage, employee, batch, 2, "tilføj");
 
+        Query transQuery = session.createQuery("From Transactions where id =:id");
 
-        new RestaurantStorage(restaurant.getId(), s.getId());
+        transQuery.setParameter("id", transactions.getId());
 
+        Transactions foundTransactions = (Transactions) transQuery.uniqueResult();
 
-        transactions.registerTransaction(s, e, b, 2, "tilføj");
+        assertTrue(foundTransactions != null);
 
-        //Establishing a connection to the database
-        Session session = new SessionFactoryCfg().getSessionFactory().openSession();
+        restaurant.removeRestaurant();
 
-        //Assuring that the transaction also is stored in the database
-        assertEquals(transactions, session.get(model.Transactions.class, transactions.getId()));
         session.close();
-
-        session = new SessionFactoryCfg().createSessionFactory().openSession();
-
-        try {
-            transaction = session.beginTransaction();
-            //Deletes the transaction from the database
-            session.delete(transactions);
-            transaction.commit();
-
-        } catch (HibernateException e) {
-            System.out.println("Something went wrong");
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-    }
-
-    @Test
-    void getName() {
-        Employee employee = new Employee();
-        employee.setFirstName("Kaj");
-        assertEquals("Kaj", employee.getFirstName());
-    }
-
-    @Test
-    void getBatch() {
-        Batch batch = new Batch();
-        batch.setBatchNumber("123KH");
-        assertEquals("123KH", batch.getBatchNumber());
-    }
-
-    @Test
-    void getProduct() {
-        Product product = new Product();
-        product.setName("Pepsi Max");
-        assertEquals("Pepsi Max", product.getName());
     }
 }
