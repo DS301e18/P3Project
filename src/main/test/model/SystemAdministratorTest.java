527fa60dda86ef58b2789059a6a5e648142d5df2
Trja17 <45165051+Trja17@users.noreply.github.com>
2018-12-10 22:50:24 +0100

    Cleaned up the SystemAdministrator class + added 100 line coverage for SystemAdministratorTest

---
+++ src/main/test/model/SystemAdministratorTest.java
@@ -0,0 +1,36 @@
+package model;
+
+import org.hibernate.Session;
+import org.junit.jupiter.api.Test;
+import util.SessionFactoryCfg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SystemAdministratorTest {
+
+    @Test
+    void collectRestaurants() {
+        Session session = new SessionFactoryCfg().createSessionFactory().openSession();
+
+        Restaurant restaurant = new Restaurant("Test Restaurant");
+
+        assertTrue(SystemAdministrator.collectRestaurants().size() >= 1);
+
+        restaurant.removeRestaurant();
+
+        session.close();
+    }
+
+    @Test
+    void collectManagers() {
+        Session session = new SessionFactoryCfg().createSessionFactory().openSession();
+
+        Manager manager = new Manager("Test", "Test", "Test", "Test");
+
+        assertTrue(SystemAdministrator.collectManagers().size() >= 1);
+
+        manager.removeManager();
+
+        session.close();
+    }
+}
