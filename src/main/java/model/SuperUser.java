181d5c7de36b0d37cfffda9724a4663b37f869d8
Trja17 <45165051+Trja17@users.noreply.github.com>
2018-12-10 10:27:36 +0100

    Lavet superbruger klasse

---
+++ src/main/java/model/SuperUser.java
@@ -0,0 +1,97 @@
+package model;
+
+public class SuperUser extends Employee {
+
+    /**
+     * Field
+     **/
+    private int id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String role = "Superbruger";
+
+    public SuperUser(String username, String password, String firstName, String lastName) {
+        super(username, password, firstName, lastName);
+
+        addObject(this);
+    }
+
+    public SuperUser() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstname='" + firstName + '\'' +
+                ", lastname='" + lastName + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+
+}
