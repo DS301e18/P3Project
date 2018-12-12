b1ca155fea3592cedf8cc31f9064dd1394161b6a
Trja17 <45165051+Trja17@users.noreply.github.com>
2018-12-12 23:22:15 +0100

    Added test for the readHistory method

---
+++ src/main/test/model/HistoryTest.java
@@ -0,0 +1,63 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import relationClasses.ProductBatch;
+import relationClasses.RestaurantEmployee;
+import relationClasses.RestaurantStorage;
+import relationClasses.StorageProduct;
+import util.AddRemove;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HistoryTest extends AddRemove {
+
+    @Test
+    void readEmployeeHistory() {
+        History history = new History();
+
+        Transactions transactions = new Transactions();
+
+        Restaurant restaurant = new Restaurant("Test Restaurant");
+        Employee employee = new Employee("Test12", "Test12", "Tester", "Testersen", "Medarbejder");
+        Storage storage = new Storage("Test Lager");
+        Product product = new Product("Product Test", 10, BigDecimal.valueOf(10));
+        Batch batch = new Batch(product, "batchNumberTest", 10);
+
+        RestaurantStorage restaurantStorage = new RestaurantStorage(restaurant.getId(), storage.getId());
+        RestaurantEmployee restaurantEmployee = new RestaurantEmployee(restaurant.getId(), employee.getId());
+        StorageProduct storageProduct = new StorageProduct(storage.getId(), product.getId());
+        ProductBatch productBatch = new ProductBatch(product.getId(), batch.getId());
+
+        transactions.registerTransaction(storage, employee, batch, 2, "tilføj");
+
+        assertTrue(history.readEmployeeHistory(employee.getFirstName() + " " + employee.getLastName()).size() >= 1);
+
+        restaurant.removeRestaurant();
+    }
+
+    @Test
+    void readHistory() {
+        History history = new History();
+
+        Transactions transactions = new Transactions();
+
+        Restaurant restaurant = new Restaurant("Test Restaurant");
+        Employee employee = new Employee("Test12", "Test12", "Tester", "Testersen", "Medarbejder");
+        Storage storage = new Storage("Test Lager");
+        Product product = new Product("Product Test", 10, BigDecimal.valueOf(10));
+        Batch batch = new Batch(product, "batchNumberTest", 10);
+
+        RestaurantStorage restaurantStorage = new RestaurantStorage(restaurant.getId(), storage.getId());
+        RestaurantEmployee restaurantEmployee = new RestaurantEmployee(restaurant.getId(), employee.getId());
+        StorageProduct storageProduct = new StorageProduct(storage.getId(), product.getId());
+        ProductBatch productBatch = new ProductBatch(product.getId(), batch.getId());
+
+        transactions.registerTransaction(storage, employee, batch, 2, "tilføj");
+
+        assertTrue(history.readHistory(1,employee.getFirstName() + " " + employee.getLastName(),storage).size() >= 1);
+
+        restaurant.removeRestaurant();
+    }
+}
