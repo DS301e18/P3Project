09858adb536b58037816646163ad29341a60d9ae
Trja17 <45165051+Trja17@users.noreply.github.com>
2018-12-18 16:07:19 +0100

    Added comments to the SystemAdministrator class

--- src/main/java/model/SystemAdministrator.java
+++ src/main/java/model/SystemAdministrator.java
@@ -13,25 +13,35 @@ public class SystemAdministrator extends AddRemove {
 
     //Returns a list of all restaurants in the database
     public static List<Restaurant> collectRestaurants() {
+        //Opens a session from sessionFactoryCfg
         Session session = SessionFactoryCfg.getSessionFactory().openSession();
+        //Makes a list with the restaurant data in the database
         List<Restaurant> restaurantList = session.createQuery("FROM Restaurant").list();
+        //Closes the session
         session.close();
+        //Returns an arraylist of all restaurant
         return restaurantList;
     }
 
     //Returns a list of all managers in the database
     public static List<Manager> collectManagers() {
+        //Empty list of managers
         List<Manager> managerList;
 
+        //Opens a session from sessionFactoryCfg
         Session session = SessionFactoryCfg.getSessionFactory().openSession();
 
+        //Query for all employees with the role as "chef"
         Query employeeQuery = session.createQuery("FROM Employee where role=:i");
         employeeQuery.setParameter("i", "Chef");
 
+        //Assigns managerList to the query list made just above
         managerList = employeeQuery.list();
 
+        //Closes the session
         session.close();
 
+        //Returns a list of all managers
         return managerList;
     }
 }
