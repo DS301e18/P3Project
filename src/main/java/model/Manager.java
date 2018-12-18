8a79fdd088bb9013ca6cbca93d92e416cee3ad59
Trja17 <45165051+Trja17@users.noreply.github.com>
2018-12-18 13:47:00 +0100

    Added comments

--- src/main/java/model/Manager.java
+++ src/main/java/model/Manager.java
@@ -2,11 +2,17 @@ package model;
 
 public class Manager extends Employee {
 
+    /**
+     * Methods
+     **/
+
+    //Constructor with database connectivity included
     public Manager(String username, String password, String firstName, String lastName) {
 
         super(username, password, firstName, lastName, "Chef");
     }
 
+    //Empty constructor because of AddRemove.
     public Manager() {
     }
 
