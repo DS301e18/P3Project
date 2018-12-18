8ff48fbbb35e476e2b30bec54f5e63beefacbf4f
Trja17 <45165051+Trja17@users.noreply.github.com>
2018-12-18 13:57:20 +0100

    Added comments

--- src/main/java/model/SuperUser.java
+++ src/main/java/model/SuperUser.java
@@ -2,12 +2,16 @@ package model;
 
 public class SuperUser extends Employee {
 
+    /**
+     * Methods
+     **/
 
+    //Constructor with database connectivity included
     public SuperUser(String username, String password, String firstName, String lastName) {
         super(username, password, firstName, lastName, "Superbruger");
     }
 
-
+    //Empty constructor because of AddRemove.
     public SuperUser() {
     }
 }
