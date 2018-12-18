d273c60a09a315879d9284078ef986f4589e5bd7
Trja17 <45165051+Trja17@users.noreply.github.com>
2018-12-18 13:43:07 +0100

    Added comments

--- src/main/test/model/TransactionsTest.java
+++ src/main/test/model/TransactionsTest.java
@@ -18,33 +18,40 @@ class TransactionsTest extends AddRemove {
 
     @Test
     void registerTransactionTest() {
+        //Open session
         Session session = SessionFactoryCfg.getSessionFactory().openSession();
 
+        //New transactions
         Transactions transactions = new Transactions();
 
+        //Add to database
         Restaurant restaurant = new Restaurant("Test Restaurant");
         Employee employee = new Employee("Test12", "Test12", "Test Person", "Test Name", "Medarbejder");
         Storage storage = new Storage("Test Lager");
         Product product = new Product("Product Test", 10, BigDecimal.valueOf(10));
         Batch batch = new Batch(product, "batchNumberTest", 10);
 
+        //Add relations to database
         RestaurantStorage restaurantStorage = new RestaurantStorage(restaurant.getId(), storage.getId());
         RestaurantEmployee restaurantEmployee = new RestaurantEmployee(restaurant.getId(), employee.getId());
         StorageProduct storageProduct = new StorageProduct(storage.getId(), product.getId());
         ProductBatch productBatch = new ProductBatch(product.getId(), batch.getId());
 
+        //Makes transaction
         transactions.registerTransaction(storage, employee, batch, 2, "tilføj");
 
+        //Query for this unique transaction and saving it as foundTransaction
         Query transQuery = session.createQuery("From Transactions where id =:id");
-
         transQuery.setParameter("id", transactions.getId());
-
         Transactions foundTransactions = (Transactions) transQuery.uniqueResult();
 
+        //assertEquals to ensure the expected is happening
         assertEquals(transactions.getId(), foundTransactions.getId());
 
+        //Removes test objects from database
         restaurant.removeRestaurant();
 
+        //Closes the session
         session.close();
     }
 }
